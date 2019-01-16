7290700bbc9c348323dc3855fdbcff9594de98ec
Andrei Babkin <t091342@gmail.com>
2019-01-17 00:16:54 +0200

    Implement request to API

--- services/update_stock.php
+++ services/update_stock.php
@@ -7,4 +7,37 @@
  * Time: 23:12
  */
 
-echo 'Hi!';
+session_start();
+
+// include ERPLY API class
+include("../utils/EAPI.class.php");
+
+// Initialise class
+$api = new EAPI();
+
+// Configuration settings
+$api->clientCode = 380579;
+$api->username = 'andrei@malachiteden.com';
+$api->password = 'x7bzWBgZqx5cLZSx';
+$api->url = "https://".$api->clientCode.".erply.com/api/";
+
+// Input parameters [{"requestName":"getProductStock","warehouseID":1,"getAmountReserved":1},{"requestName":"getProductStock","warehouseID":2}]
+$requests = [
+    ['requestName' => 'getProductStock', 'warehouseID' => 1],
+    ['requestName' => 'getProductStock', 'warehouseID' => 2]
+];
+$params = [
+    'requests' => json_encode($requests)
+];
+
+// Get stock amounts from ERPLY API
+$result = 'Fail';
+try{
+    $result = $api->sendRequest(false, $params);
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+$output = json_decode($result, true);
+
+print_r($output);
