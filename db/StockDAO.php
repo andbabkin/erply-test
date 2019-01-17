0a9579520e9e7f3a4b331dc7fca79aeda44a9b63
Andrei Babkin <t091342@gmail.com>
2019-01-17 20:41:06 +0200

    Add methods

--- db/StockDAO.php
+++ db/StockDAO.php
@@ -22,7 +22,7 @@ class StockDAO
         $sql = "SELECT * FROM `{$this->table}`";
         $stmt = DBConnection::getConn()->query($sql);
 
-        while($row = $stmt->fetch(\PDO::FETCH_ASSOC)){
+        while($row = $stmt->fetch()){
             if(!array_key_exists($row[$this->stock], $data)){
                 $data[$this->stock] = [];
             }
@@ -30,5 +30,52 @@ class StockDAO
         }
 
         return $data;
+    }
+
+    /**
+     * @return int number of rows deleted
+     */
+    public function deleteAll()
+    {
+        $sql = "DELETE FROM `{$this->table}`";
+        return DBConnection::getConn()->exec($sql);
+    }
+
+    /**
+     * @param array $data - [ [warehouseID] => [ ['productID' => integer,'amountInStock' => decimal(,6)], ... ], ... ]
+     * @return int number of added rows
+     */
+    public function insertFromExternalSource($data)
+    {
+        $first = true;
+        $sql = "INSERT INTO `{$this->table}` (`{$this->id}`,`{$this->qty}`,`{$this->stock}`) VALUES";
+        foreach ($data as $stock => $items){
+            $s = (int)$stock;
+            foreach ($items as $record){
+                if($first){
+                    $first = false;
+                } else {
+                    $sql .= ',';
+                }
+                $p = (int)$record['productID'];
+                $a = $record['amountInStock'] + 0; // make number if type is string
+                $sql .= "($p, $a, $s)";
+            }
+        }
+        return DBConnection::getConn()->exec($sql);
+    }
+
+    public function insert($product_id, $amount, $stock_id)
+    {
+        $sql = "INSERT INTO `{$this->table}` (`{$this->id}`,`{$this->qty}`,`{$this->stock}`) VALUES"
+            .'('.(int)$product_id.','.($amount+0).','.(int)$stock_id.')';
+        return DBConnection::getConn()->exec($sql);
+    }
+
+    public function update($product_id, $amount, $stock_id)
+    {
+        $sql = "UPDATE `{$this->table}` SET `{$this->qty}`=".($amount+0)
+            ." WHERE `{$this->id}`=".(int)$product_id." AND `{$this->stock}`=".(int)$stock_id;
+        return DBConnection::getConn()->exec($sql);
     }
 }
