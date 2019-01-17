dc96bafb85158d16270aabcdf863b784dd232cd7
Andrei Babkin <t091342@gmail.com>
2019-01-17 15:51:35 +0200

    Move column names to variables

--- db/ParametersDAO.php
+++ db/ParametersDAO.php
@@ -4,11 +4,16 @@ namespace DB;
 
 class ParametersDAO
 {
+    // table name
     private $table = 'parameters';
+
+    // column names
+    private $name = 'name';
+    private $value = 'value';
 
     public function get($name, $create_if_not_exist = true)
     {
-        $sql = "SELECT `value` FROM `{$this->table}` WHERE `name` = ?";
+        $sql = "SELECT `{$this->value}` FROM `{$this->table}` WHERE `{$this->name}` = ?";
         $stmt = DBConnection::executeStatement($sql, [$name]);
         $param = $stmt->fetch(\PDO::FETCH_ASSOC);
         if($param === false){
@@ -17,19 +22,19 @@ class ParametersDAO
             }
             return null;
         } else {
-            return $param['value'];
+            return $param[$this->value];
         }
     }
 
     public function create($name)
     {
-        $sql = "INSERT INTO {$this->table} (`name`) VALUES (?)";
+        $sql = "INSERT INTO {$this->table} (`{$this->name}`) VALUES (?)";
         DBConnection::executeStatement($sql, [$name]);
     }
 
     public function set($name, $value)
     {
-        $sql = "UPDATE {$this->table} SET `value` = ? WHERE `name` = ?";
+        $sql = "UPDATE {$this->table} SET `{$this->value}` = ? WHERE `{$this->name}` = ?";
         DBConnection::executeStatement($sql, [$value, $name]);
     }
 }
